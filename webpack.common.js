833fc1e094aa0a8196b4a4539be69d8941cc220d
Omoefe Dukuye <omoefe.dukuye@gmail.com>
2019-02-27 14:43:02 +0100

    Set up production webpack build

---
+++ webpack.common.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const ExtractTextPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      loader: 'babel-loader',
+      test: /\.js$/,
+      exclude: /node_modules/
+    }, {
+      test: /\.s?css$/,
+      use: [
+        ExtractTextPlugin.loader, {
+          loader: 'css-loader',
+          options: {
+            sourceMap: true
+          }
+        }, {
+          loader: 'sass-loader',
+          options: {
+            sourceMap: true
+          }
+        }
+      ]
+    }]
+  },
+  plugins: [
+    new ExtractTextPlugin({ filename: 'styles.css' })
+  ]
+};
